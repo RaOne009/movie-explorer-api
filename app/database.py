604f919689aceac7dacdd02336e9e725bb850d2e
RaOne009 <rs1257933@gmail.com>
2025-07-26 05:49:42 +0530

    .env file added and fix db connection issue

--- app/database.py
+++ app/database.py
@@ -1,13 +1,26 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+import time
 
-DATABASE_URL = "postgresql://postgres:147896@localhost/FastApi"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/movie_db")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+while True:
+    try:
+        with engine.connect() as connection:
+            print("Connected to the database successfully.")
+            break
+    except Exception as e:
+        print(f"Failed to connect to the database: {e}. Retrying in 5 seconds...")
+        time.sleep(5)
+        
 Base = declarative_base()
 
 def get_db():
